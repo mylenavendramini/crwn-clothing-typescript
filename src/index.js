d839e4838443b982aa72844e7b25ed1f833a5781
mylenavendramini <myvendramini@gmail.com>
2022-06-16 10:21:26 +0100

    add procfile

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import { Elements } from "@stripe/react-stripe-js";
 import { store, persistor } from "./store/store";
 import "./index.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { stripePromise } from "./utils/stripe/stripe.utils";
 
 const rootElement = document.getElementById("root");
@@ -35,8 +34,3 @@ render(
   </React.StrictMode>,
   rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
